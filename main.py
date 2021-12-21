7385bb360a5d07dfc05e86af722d8f965d1fb11d
Renzofbn <renzofbn@gmail.com>
2021-12-21 00:37:04 -0500

    Added ascii status

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+import os
+from Data.ascii_status import *
+
+
 def run():
-    pass
+    def clear():
+        if os.name == "nt":
+            os.system("cls")
+        else:
+            os.system("clear")
+
+    for i in range(11):
+        clear()
+        hang_py(i)
+        input("")
 
 
 if __name__ == '__main__':
