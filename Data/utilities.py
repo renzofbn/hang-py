bee21b59cf2d5b7515f62c026b6f6a8b1ba5abbb
Renzofbn <renzofbn@gmail.com>
2021-12-22 16:02:18 -0500

    exception handling

--- Data/utilities.py
+++ Data/utilities.py
@@ -14,7 +14,7 @@ def player(name):
     print("Las reglas son simples, tienes 10 intentos para adivinar la totalidad de letras ")
     print("que conforman una palabra, escogida al azar")
     print()
-    input("¿List@ para jugar?  (Presiona enter para continuar)  ")
+    input('\033[93m¿List@ para jugar?  (Presiona enter para continuar)  \033[0m')
 
 
 def map_word(word):
@@ -27,3 +27,55 @@ def map_word(word):
             dicc[caracter] = [cont]
         cont += 1
     return dicc
+
+
+def get_name():
+    while True:
+        try:
+            name = input("\033[94mPorfavor ingresa tu nombre: \033[0m")
+            if len(name) == 0:
+                raise ValueError("\033[91mNo se permiten espacios en blaco, intenta de nuevo....")
+            break
+        except ValueError as ve:
+            print(ve)
+            input("\033[93mPresiona enter para continuar ")
+            print('\033[0m')
+    return name
+
+
+def right_c(chars):
+    while True:
+        try:
+            c = input('\033[94mIngresa una letra: \033[0m').upper()
+            if c.isdigit():
+                raise ValueError("Solo se acpetan letras, ten cuidado!!")
+            if len(c) > 1:
+                raise ValueError("Ingresa una letra por vez, ten cuidado!!")
+            if len(c) == 0:
+                raise ValueError("No se aceptan respuestas en blanco, acaso quieres perder?")
+            if c in chars:
+                raise ValueError("Ya has ingresado esta letra anteriormente, ten cuidado!!")
+            if not c.isalpha():
+                raise ValueError("No se permiten caracteres, ten cuidado!!")
+            break
+        except ValueError as ve:
+            print(f'\033[91m{ve}')
+            input('\033[93m' + "Presiona enter para continuar " + '\033[0m')
+            print()
+    return c
+
+
+def show_chars(chars, r_chars, w_chars):
+    print("Letras ingresadas hasta ahora -> ", end="")
+    for letra in chars:
+        print(letra, end=" ")
+    print()
+    print("Letras correctas ingresadas hasta ahora -> ", end="")
+    for letra in r_chars:
+        print(letra, end=" ")
+    print()
+    print("Letras incorrectas ingresadas hasta ahora -> ", end="")
+    for letra in w_chars:
+        print(letra, end=" ")
+    print('\n')
+
--- main.py
+++ main.py
@@ -5,43 +5,44 @@ import random
 
 def run():
     tittle()
-    print("¡ Bienvenid@ !")
-    name = input("Porfavor ingresa tu nombre: ")
+    print("¡ Bienvenid@ ! \n")
+    name = get_name()
     player(name)
     clear()
-    errores = 0
     with open("./Data/words.txt", 'r', encoding="utf-8") as file:
         words = [palabra[:-1] for palabra in file]
     guess = random.choice(words)
     right_a = [c for c in guess]
     dicc = map_word(guess)
-    lines = ["_" for _ in range(len(guess))]
-    chars = []
+    lines = [" _ " for _ in range(len(guess))]
+    chars, r_chars, w_chars, errores = [], [], [], 0
+    state = 1   # 1 is for game, 2 is for correct answer, 3 is for leave game
     while True:
-        if errores < 10:
+        if errores < 10 and state == 1:
             hang_py(errores)
             print("¡Adivina la palabra¡")
+            print()
             for line in lines:
                 print(line, end="")
-            print()
+            print('\n')
 
-            print("Letra ingresadas hasta ahora -> ", end="")
-            for letra in chars:
-                print(letra, end=" ")
-            print()
-
-            c = input("Ingresa una letra: ").upper()
+            show_chars(chars, r_chars, w_chars)
+            c = right_c(chars)
             chars.append(c)
             if dicc.get(c) is not None:
+                r_chars.append(c)
                 for a in dicc[c]:
                     lines[a] = c
                 if lines == right_a:
                     clear()
-                    errores=20
+                    state= 2
+                print('\033[92m')
             else:
+                print('\033[91m')
+                w_chars.append(c)
                 errores += 1
             clear()
-        elif errores == 20:
+        elif state == 2:
             clear()
             hang_py(errores)
             print("Bien jugado, adivinaste la palabra", guess)
@@ -49,16 +50,15 @@ def run():
             for letra in chars:
                 print(letra, end=" ")
             print()
-            again = input("¿ Quieres seguir jugando ?  [s/n] ")
+            again = input("\033[93m\n¿ Quieres seguir jugando ?  [s/n] \033[0m")
             if again == "s" or again == "S":
                 guess = random.choice(words)
                 right_a = [c for c in guess]
                 dicc = map_word(guess)
-                lines = ["_" for _ in range(len(guess))]
-                chars = []
-                errores = 0
+                lines = [" _ " for _ in range(len(guess))]
+                chars, r_chars, w_chars, errores, state = [], [], [], 0, 1
             else:
-                errores=100
+                state = 3
             clear()
         else:
             break
@@ -66,17 +66,17 @@ def run():
     if errores == 10:
         hang_py(errores)
         print("¡No pudiste adivinar la palabra!", "\n")
-        print(f"La palabra era: {guess}", "\n")
+        print(f"\033[95mLa palabra era: {guess}\033[0m", "\n")
         print("Las letras que ingresaste fueron -> ", end="")
         for letra in chars:
             print(letra, end=" ")
         print()
         print(f"{name}, gracia por jugar, hasta otra oportunidad", "\n" * 2)
-        input("Presiona enter para continuar ")
+        input("\033[93mPresiona enter para continuar \033[0m")
         print('\033[91m')
     else:
-        print("Que tengas buen día, gracias por jugar")
-        input("Presiona enter para continuar ")
+        print("Que tengas buen día, gracias por jugar\n")
+        input("\033[93mPresiona enter para continuar ")
         print('\033[92m')
     clear()
     game_over()
