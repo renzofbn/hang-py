7a38fadb1a3811e83c18399eed4ff1e4dc03c06b
Renzo Valentin <renzofbn@gmail.com>
2021-12-25 21:27:55 -0500

    Uppdate Win-Updater

--- Windows_Updater.bat
+++ Windows_Updater.bat
@@ -1,3 +1,3 @@
 @echo off
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
-git pull origin main && copy hangpy.bat %windir%\System32 && exit
+cd %HOMEDRIVE%\Games\hang-py && git pull origin main && copy hangpy.bat %windir%\System32 && exit
