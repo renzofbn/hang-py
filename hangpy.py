8fc64e68b85359a813c0969fbdb25e3b24c2acf5
Renzo <renzofbn@tutanota.com>
2024-02-01 23:21:14 -0500

    v1.0 Multilenguaje es/en

--- hangpy.py
+++ hangpy.py
@@ -1,13 +1,23 @@
-from src.ascii_status import tittle
-from src.utilities import get_name, clear, main_game, show_top
-
+from src.game import start_game, show_top, configure_game
+from src.utils import clear
+import argparse
 
 def run():
-    clear()
-    tittle()
-    name = get_name(name="")
-    clear()
-    main_game(name)
+    try:
+        parser = argparse.ArgumentParser()
+        parser.add_argument("-c", "--config", help="Configure game settings", action="store_true")
+        parser.add_argument("-t", "--top", help="Show top 5 players", action="store_true")
+        args = parser.parse_args()
+        if args.config:
+            configure_game()
+        elif args.top:
+            show_top()
+        else:
+            start_game()
+
+    except KeyboardInterrupt:
+        clear()
+        print("\033[91mBye!\033[0m")
 
 
 if __name__ == '__main__':
---
+++ src/utils.py
@@ -0,0 +1,47 @@
+from configparser import ConfigParser
+
+def gen_config(config: list = False):
+    if not config:
+        config_file = ConfigParser()
+        config_file["Game"] = {
+                "lang": "es",
+                "word_lang": "es",
+                "default_player": "",
+            }
+
+        config_file["Player_1"] = {
+                "name": "Player 1",
+                "points": "0",
+            }
+        config_file["Player_2"] = {
+                "name": "Player 2",
+                "points": "0",
+            }
+        config_file["Player_3"] = {
+                "name": "Player 3",
+                "points": "0",
+            }
+        config_file["Player_4"] = {
+                "name": "Player 4",
+                "points": "0",
+            }
+        config_file["Player_5"] = {
+                "name": "Player 5",
+                "points": "0",
+            }
+    with open('./src/config.ini', 'w') as file_object:
+        config_file.write(file_object)
+
+
+def clear():
+    from os import system, name
+    if name == "nt":
+        system("cls")
+    else:
+        system("clear")
+
+CONFIG = ConfigParser()
+CONFIG.read('./src/config.ini')
+if not CONFIG.sections():
+    gen_config()
+    CONFIG.read('./src/config.ini')
